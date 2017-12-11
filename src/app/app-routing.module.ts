dfb2131df27f1ebcff9929bc639d0fed8d5d64a8
Natan Br <Natan Br>
2017-12-10 22:50:36 -0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResumeComponent } from './pages/resume';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ResumeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './pages/resume';
+import { HeaderComponent } from './pages/resume/header';
+import { BodyComponent } from './pages/resume/body';
 
+import { DalService } from './services';
+import { CardComponent } from './pages/resume/body/card/card.component';
+import { TagComponent } from './pages/resume/body/card/tag/tag.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    HeaderComponent,
+    BodyComponent,
+    CardComponent,
+    TagComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
